931a563eb022ca334820d2ed26c414ca72940302
Max Gonzih <gonzih@gmail.com>
2012-06-04 21:50:30 +0300

    add button, cleanup code

--- src/clojure_arduino/core.clj
+++ src/clojure_arduino/core.clj
@@ -7,26 +7,47 @@
 
 (def cnt (ref 0))
 (def last-id (ref nil))
-(def board (arduino :firmata "/dev/ttyACM0"))
-(def ard (agent board))
+(def last-id-pressed (atom nil))
+(def contacting-gmail (atom false))
 
-(defn check-mail [board]
-  (digital-write board 11 HIGH)
+(def board (arduino :firmata "/dev/ttyACM0"))
+
+(defn check-mail []
+  (reset! contacting-gmail true)
   (dosync
-    (ref-set cnt (gmail/unread-count)))
-  (digital-write board 11 LOW))
+    (ref-set cnt (gmail/unread-count))
+    (ref-set last-id (gmail/last-message-id)))
+  (reset! contacting-gmail false))
+
+(defn button-pressed []
+  (digital-write board 12 LOW)
+  (reset! last-id-pressed @last-id))
+
+(defn button-down? []
+  (= (digital-read board 3) 1))
 
 (defn -main []
   (pin-mode board 12 OUTPUT)
   (pin-mode board 11 OUTPUT)
-  (enable-pin board :digital 3)
   (pin-mode board 3  INPUT)
-  ;(digital-read board 3)
+  (enable-pin board :digital 3)
+
   (future
-    (send-off ard check-mail)
-    (Thread/sleep (* 3 60 1000)))
+    (while true
+      (check-mail)
+      (Thread/sleep (* 5 1000))))
+
   (while true
-    (if (> @cnt 0)
+    (when (button-down?)
+      (Thread/sleep 300)
+      (when (button-down?)
+        (button-pressed)))
+
+    (if (and (> @cnt 0) (not= @last-id @last-id-pressed))
       (digital-write board 12 HIGH)
-      (digital-write board 12 LOW)))
+      (digital-write board 12 LOW))
+
+    (if @contacting-gmail
+      (digital-write board 11 HIGH)
+      (digital-write board 11 LOW)))
   (close board))
